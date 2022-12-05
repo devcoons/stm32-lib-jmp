c4164573142e882f4265ca175ed052e641ee2d92
devcoons <i_-_-_s@outlook.com>
2022-12-05 18:55:12 +0100

    upd

--- lib_jmp.c
+++ lib_jmp.c
@@ -5,7 +5,7 @@
 	---------------------------------------------------------------------------
 
 	MIT License
-	Copyright (c) 2019 Energica Motor Company (Io. D)
+	Copyright (c) 2018 Ioannis Deligiannis | Devcoons Blog
 
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
